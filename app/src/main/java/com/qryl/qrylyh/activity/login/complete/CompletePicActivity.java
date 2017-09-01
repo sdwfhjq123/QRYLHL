002009958023d39b59425c080ebea98b7df40beb
yinhao <sdwfhjq123@qq.com>
2017-09-01 17:47:48 +0800

    创建了一个上传图片的activity

---
+++ app/src/main/java/com/qryl/qrylyh/activity/login/complete/CompletePicActivity.java
@@ -0,0 +1,15 @@
+package com.qryl.qrylyh.activity.login.complete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qryl.qrylyh.R;
+
+public class CompletePicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_pic);
+    }
+}
